36fde0c7083bf73181d6645432451454da5948f8
javier <javichuela1228@gmail.com>
2023-10-02 20:09:33 -0300

    firs commit

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+		nameProduct: 'Pikachu',
+		price: 60,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+		nameProduct: 'Charmander',
+		price: 110,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/1.png',
+		nameProduct: 'Bulbasaur',
+		price: 400,
+		quantity: 1,
+		
+	},
+	{
+		id: 4,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/54.png',
+		nameProduct: 'Psyduck',
+		price: 120,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/134.png',
+		nameProduct: 'Vaporeon',
+		price: 220,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/26.png',
+		nameProduct: 'Raichu',
+		price: 300,
+		quantity: 1,
+		
+	},
+	
+	// Aquí pueden agregar más o modificar los productos existentes
+];
